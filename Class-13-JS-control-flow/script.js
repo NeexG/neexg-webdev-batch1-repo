415df929f062b9379ac03148986d8974fd07b30d
rakibjrb <rakibjrb572157@gmail.com>
2026-07-22 23:31:07 +0600

    updated

---
+++ Class-13-JS-control-flow/script.js
@@ -0,0 +1,149 @@
+// const mark = 85;
+
+// if (condition) {
+//     // do this
+// } else {
+//     // do this
+// }
+
+// if (mark >= 80) {
+//     console.log("You've got A+");
+// } else {
+//     console.log("You did not get A+");
+// }
+
+// if (condition) {
+//     // do this
+// } else if (condition) {
+//     // do this
+// } else if (condition) {
+//     // do this
+// } else {
+//     // do this
+// }
+
+// if (mark > 100) {
+//     console.log("Enter valid mark")
+// } else if (mark >= 80 && mark <= 100) {
+//     console.log("You've got A+")
+// } else if (mark >= 70 && mark <= 79) {
+//     console.log("You've got A")
+// } else if (mark >= 60 && mark <= 69) {
+//     console.log("You've got A-")
+// } else if (mark >= 50 && mark <= 59) {
+//     console.log("You've got B")
+// } else if (mark >= 40 && mark <= 49) {
+//     console.log("You've got C")
+// } else if (mark >= 33 && mark <= 39) {
+//     console.log("You've got D")
+// } else {
+//     console.log("You failed")
+// }
+
+// const number1 = Number(prompt("Enter Number 1 : "));
+// const number2 = Number(prompt("Enter Number 2 : "));
+// const number3 = Number(prompt("Enter Number 3 : "));
+
+// if (number1 > number2) {
+//     console.log(number1, number2, number3);
+//     if (number1 > number3) {
+//         console.log("Biggest is : ", number1);
+//     } else {
+//         console.log("Biggest is : ", number3);
+//     }
+// } else {
+//     if (number2 > number3) {
+//         console.log(number1, number2, number3);
+//         console.log("Biggest is : ", number2);
+//     } else {
+//         console.log(number1, number2, number3);
+//         console.log("Biggest is : ", number3);
+//     }
+// }
+
+// switch(value) {
+//     case condition:
+//         // do this
+//         break;
+//     case condition:
+//         // do this
+//         break;
+//     case condition:
+//         // do this
+//         break;
+//     case condition:
+//         // do this
+//         break;
+//     case condition:
+//         // do this
+//         break;
+//     case condition:
+//         // do this
+//         break;
+//     default:
+//         // do this
+//         break;
+// }
+
+const day = 6;
+
+switch (day) {
+    case 1:
+        console.log("Saturday");
+        break;
+    case 2:
+        console.log("Sunday");
+        break;
+    case 3:
+        console.log("Monday");
+        break;
+    case 4:
+        console.log("Tuesday");
+        break;
+    case 5:
+        console.log("Wednesday");
+        break;
+    case 6:
+        console.log("Thursday");
+        break;
+    case 7:
+        console.log("Friday");
+        break;
+    default:
+        console.log("Invalid day");
+        break;
+}
+
+// if (day === 1) {
+//     console.log("Saturday");
+// } else if (day === 2) {
+//     console.log("Sunday");
+// } else if (day === 3) {
+//     console.log("Monday");
+// } else if (day === 4) {
+//     console.log("Tuesday");
+// } else if (day === 5) {
+//     console.log("Wednesday");
+// } else if (day === 6) {
+//     console.log("Thursday");
+// } else if (day === 7) {
+//     console.log("Friday");
+// } else {
+//     console.log("Invalid day");
+// }
+
+// const user = {
+//     id: 1,
+//     name: "Rakibul",
+//     email: "email@gmail.com",
+//     password: "1123234"
+// }
+
+// const newInfo = {
+//     ...user,
+//     location: "Sherpur",
+//     role: "Sr Developer",
+//     description: "Gamer and also developer, Love coding"
+// }
+
+// console.log(newInfo)
